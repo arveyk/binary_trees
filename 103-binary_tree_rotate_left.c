6fddeadd83eadb57f39e0ae4513f86421de9084d
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-01 17:24:10 +0300

    if tree is NULL

--- 103-binary_tree_rotate_left.c
+++ 103-binary_tree_rotate_left.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stddef.h>
 
 /**
  * binary_tree_rotate_left - rotates a binary tree to the left
@@ -10,7 +11,11 @@ binary_tree_t *binary_tree_rotate_left(binary_tree_t *tree)
 {
 	binary_tree_t *new_root;
 
+	if (tree == NULL)
+		return (NULL);
 	new_root = tree->right;
+	if (new_root == NULL)
+		return (tree);
 	tree->right = new_root->left;
 	new_root->left = tree;
 	return (new_root);
--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -1,4 +1,5 @@
 #include "binary_trees.h"
+#include <stddef.h>
 
 /**
  * binary_tree_rotate_left - rotates a binary tree to the left
@@ -10,7 +11,11 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 {
 	binary_tree_t *new_root;
 
+	if (tree == NULL)
+		return (NULL);
 	new_root = tree->left;
+	if (new_root == NULL)
+		return (tree);
 	tree->left = new_root->right;
 	new_root->right = tree;
 	return (new_root);
