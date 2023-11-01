6b2cb36261362b9e3c51c1c298ca730251bad246
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-01 17:25:49 +0300

    betty style

--- 104-binary_tree_rotate_right.c
+++ 104-binary_tree_rotate_right.c
@@ -2,7 +2,7 @@
 #include <stddef.h>
 
 /**
- * binary_tree_rotate_left - rotates a binary tree to the left
+ * binary_tree_rotate_right - rotates a binary tree to the left
  * @tree: pointer to the root of the tree
  *
  * Return: pointer to the new root
@@ -15,7 +15,7 @@ binary_tree_t *binary_tree_rotate_right(binary_tree_t *tree)
 		return (NULL);
 	new_root = tree->left;
 	if (new_root == NULL)
-		return (tree);
+		return (NULL);
 	tree->left = new_root->right;
 	new_root->right = tree;
 	return (new_root);
