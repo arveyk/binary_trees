742c7e76737d1a1d80e0dd188829bf16a960d63f
Harvey W. Kis <harveykisiangani@gmail.com>
2023-11-03 16:46:59 +0300

    parent should not be checked if equal to null

--- 0-binary_tree_node.c
+++ 0-binary_tree_node.c
@@ -12,9 +12,6 @@ binary_tree_t *binary_tree_node(binary_tree_t *parent, int value)
 {
 	binary_tree_t *new_node;
 
-	if (parent == NULL)
-		return (NULL);
-
 	new_node = malloc(sizeof(binary_tree_t));
 	if (new_node == NULL)
 		return (NULL);
